cc532a4813a5cf1d4fdc1a942190836a2df7dfd6
emperorlinc <olowoyojames38@gmail.com>
2025-07-02 09:22:57 +0100

    Added login and logout functionalities and templates.

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,9 @@ app_name = "core"
 
 
 urlpatterns = [
+    path("", views.index_view, name="index"),
     path("register/", views.register_view, name="register"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
     path("sale/", views.sales_view, name="sales"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
@@ -7,6 +8,13 @@ from core.models import User
 # Create your views here.
 
 
+def index_view(request):
+    """
+    This view should grab some items from the database to display on the webpage as ads for the company.
+    """
+    return render(request, "index.html")
+
+
 def register_view(request):
     if request.method == "POST":
         email = request.POST.get("email")
@@ -14,10 +22,10 @@ def register_view(request):
         password = request.POST.get("password")
         confirm_password = request.POST.get("confirm_password")
 
-        if User.objects.get(email=email):
+        if User.objects.filter(email=email).exists():
             messages.error(request, "Email has already been used.")
             return redirect("core:register")
-        elif User.objects.get(name=name):
+        elif User.objects.filter(name=name).exists():
             messages.error(
                 request, "Name conflict with a name in the database.")
             return redirect("core:register")
@@ -26,10 +34,35 @@ def register_view(request):
         else:
             user = User.objects.create_user(
                 email=email, name=name, password=password)
+            user.save()
             login(request, user)
             return redirect("core:sales")
     return render(request, "core/register.html")
 
 
+def login_view(request):
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        try:
+            User.objects.get(email=email)
+        except:
+            messages.error(request, "No user with the provided email.")
+            return redirect("core:login")
+        user = authenticate(request, email=email, password=password)
+        login(request, user)
+        messages.success(request, "User logged in successfully.")
+        return redirect("core:sales")
+    return render(request, "core/login.html")
+
+
+def logout_view(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect("core:index")
+    return render(request, "core/logout.html")
+
+
 def sales_view(request):
-    pass
+    return render(request, "core/sales_list.html")
