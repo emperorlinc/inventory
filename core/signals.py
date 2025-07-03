60ef797f56ba6e2de7c4a296aca8154c90f160fe
emperorlinc <olowoyojames38@gmail.com>
2025-07-03 09:43:50 +0100

    Added signal on the sale model to update the product total quantity whenever a sale is made.

--- core/signals.py
+++ core/signals.py
@@ -1,3 +1,22 @@
 """
     Create a signal that automatically deduct the quantity product sold from the initial total quantity.
 """
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.shortcuts import get_object_or_404
+
+from core.forms import ProductForm
+from core.models import Product, Sale
+
+
+@receiver(post_save, sender=Sale)
+def total_quantity_handler(sender, instance, created, **kwargs):
+    if created:
+        product = get_object_or_404(Product, name=instance)
+        product.quantity -= instance.quantity
+        form = ProductForm(instance=product)
+        if form.is_valid():
+            form.save()
+            return "Total quantity for product has been updated."
+        return "Total quantity for product could not be updated due to invalid form."
--- inventory/settings.py
+++ inventory/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'core',
+    'core.app.CoreConfig',
 
     'django.contrib.admin',
     'django.contrib.auth',
